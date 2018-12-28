1a0f40125684575490c4160919c0742d16565370
Petro Havrylei <petro.havrylei@advancedmd.com>
2018-12-28 17:21:05 +0200

    init

---
+++ EducationEntityFramework/DataReaders.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace EducationEntityFramework {
+  internal class DataReaders {
+    private readonly string _connString;
+
+    public DataReaders(string connString) {
+      _connString = connString;
+    }
+
+    public void DataReaderProcess() {
+      SqlConnection conn = new SqlConnection(_connString);
+      var com = new SqlCommand("SELECT ID, Title FROM Nodes;SELECT ID, Login FROM Users", conn);
+
+      using (conn) {
+        conn.Open();
+
+        SqlDataReader reader = com.ExecuteReader();
+
+        while (reader.Read()) {
+          Console.WriteLine($"Id: {reader.GetInt32(0)}\tTitle: {reader.GetString(1)}");
+        }
+
+        Console.WriteLine($"\n\n");
+
+        reader.NextResult();
+
+        while (reader.Read()) {
+          Console.WriteLine($"Id: {reader.GetInt32(0)}\tTitle: {reader.GetString(1)}");
+        }
+      }
+    }
+
+    public void DataAdapterProcess() {
+      SqlConnection conn = new SqlConnection(_connString);
+
+      DataTable table = new DataTable("allPrograms");
+
+      using (conn) {
+        var command = "SELECT ID, Title FROM Nodes";
+
+        using (var cmd = new SqlCommand(command, conn)) {
+          SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+
+          conn.Open();
+          adapt.Fill(table);
+        }
+      }
+
+      foreach (DataRow row in table.Rows) {
+        foreach (DataColumn col in table.Columns) {
+          Console.Write($"{col}: {row[col]} ");
+        }
+        Console.WriteLine($"\n");
+      }
+    }
+
+    public void DataSetProcess() {
+      SqlConnection conn = new SqlConnection(_connString);
+
+      DataSet set = new DataSet("allPrograms");
+
+      using (conn) {
+        var command = "SELECT ID, Title FROM Nodes;SELECT ID, Login FROM Users";
+
+        using (var cmd = new SqlCommand(command, conn)) {
+          SqlDataAdapter adapt = new SqlDataAdapter(cmd);
+
+          conn.Open();
+          adapt.Fill(set);
+        }
+      }
+
+      foreach (DataTable table in set.Tables) {
+        Console.WriteLine("Table " + table);
+        foreach (DataRow row in table.Rows) {
+          foreach (DataColumn col in table.Columns) {
+            Console.Write($"{col}: {row[col]} ");
+          }
+          Console.WriteLine();
+        }
+        Console.WriteLine("\n");
+      }
+    }
+  }
+}
---
+++ EducationEntityFramework/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace EducationEntityFramework {
+  class Program {
+    private const string CONNECTION_STRING = "Data Source=IF979;Integrated Security=True;Database=Education";
+
+    static void Main(string[] args) {
+      //Context context = new Context();
+
+      //foreach(var temp in context.Nodes) {
+      //  Console.WriteLine(temp.Title);
+      //}
+
+      DataReaders data = new DataReaders(CONNECTION_STRING);
+
+      //data.DataReaderProcess();
+      //data.DataAdapterProcess();
+      data.DataSetProcess();
+
+      Console.ReadKey();
+    }
+  }
+
+  class Context : DbContext {
+    private readonly string _connString;
+
+    public Context(string connString) {
+      _connString = connString;
+    }
+
+    public DbSet<Node> Nodes { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+      optionsBuilder.UseSqlServer(_connString);
+    }
+  }
+
+  [Table("Users")]
+  class User {
+    public int Id { get; set; }
+
+    public string Login { get; set; }
+  }
+
+  [Table("Nodes")]
+  class Node {
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+  }
+}
